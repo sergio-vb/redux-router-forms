53093be2f893f853417172a28a176e0a65c59e0a
Svb home <sergio.valverde.b@gmail.com>
2017-09-19 20:42:53 -0600

    Working on forms functionality

--- src/components/PostsNew.jsx
+++ src/components/PostsNew.jsx
@@ -12,20 +12,27 @@ class PostsNew extends React.Component{
                     type="text" 
                     {...field.input}
                 />
+                {field.meta.touched ? field.meta.error : null}
             </div>
         );
     }
+
+    onSubmit(values){
+        console.log(values);
+    }
+
     render(){
+        const { handleSubmit } = this.props;
         return (
-            <form action="">
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                     label="Title"
                     name="title"
                     component={this.renderField}
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -33,11 +40,30 @@ class PostsNew extends React.Component{
                     name="content"
                     component={this.renderField}
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
 }
 
+function validate(values) { //Called automatically at various moments, like when submitting
+    const errors = {};
+
+    //Validate the inputs from 'values';
+    if(!values.title){
+        errors.title = "Please enter a title.";
+    }
+    if(!values.categories){
+        errors.categories = "Please enter the categories.";
+    }
+    if(!values.content){
+        errors.content = "Please enter the content.";
+    }
+
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm' //Define the 'name' of the form, to identify it
 })(PostsNew);
